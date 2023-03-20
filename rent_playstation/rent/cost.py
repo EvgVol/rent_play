f5bf237964fe42914df00c428e45dabc2325b4e2
Evgeniy V <volochek93@yandex.ru>
2023-03-20 09:19:33 +0400

    создаем новую структуру приложения

--- rent_playstation/rent/cost.py
+++
@@ -1,8 +0,0 @@
-
-
-from .models import Console
-
-
-console_1 = Console().time()
-
-print(console_1)
--- rent_playstation/rent/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_not_empty(value):
-    if value == '':
-        raise ValidationError(
-            'Параметр является обязательным',
-            params={'value': value},
-        )
