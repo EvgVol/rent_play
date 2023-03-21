25d11770f5e4fd84419b269758b84ca551d3090f
Evgeniy V <volochek93@yandex.ru>
2023-03-21 15:27:29 +0400

    добавлены данные в админку

--- backend/games/admin.py
+++ backend/games/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
-from .models import Game, Tag
+from .models import Game, Tag, Comment, Review
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    """Админка страницы тегов."""
+
     list_display = ('id', 'name', 'color', 'slug',)
-    # search_fields = ('name', 'slug',)
-    # ordering = ('name',)
+    search_fields = ('name', 'slug',)
+    ordering = ('name',)
 
 
 @admin.register(Game)
@@ -29,3 +31,29 @@ class GameAdmin(admin.ModelAdmin):
     def get_tags(self, obj):
         """Получаем теги."""
         return ', '.join(_.name for _ in obj.tags.all())
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Админка страницы отзывов."""
+
+    list_display = (
+        'pk', 'game', 'text',
+        'author', 'score', 'pub_date',
+    )
+    search_fields = ('game', 'author', 'pub_date',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Админка страницы комментариев."""
+
+    list_display = (
+        'pk', 'review', 'text',
+        'author', 'pub_date',
+    )
+    search_fields = ('review', 'author', 'pub_date',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
