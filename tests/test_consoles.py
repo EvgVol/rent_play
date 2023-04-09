3fbfbfbe79be78d470d6b8634ff75f0a7877b524
Евгений В <volochek93@yandex.ru>
2023-04-09 22:05:33 +0400

    test created consoles

--- tests/test_consoles.py
+++ tests/test_consoles.py
@@ -17,4 +17,11 @@ class Test05ConsolesAPI:
             ' страница `/api/consoles/` возвращает статус 200'
         )
 
-    
+    @pytest.mark.django_db(transaction=True)
+    def test_02_create_consoles(self, auth_client_1):
+        empty_data = {}
+        response = auth_client_1.post('/api/consoles/', data=empty_data)
+        assert response.status_code == 400, (
+            'Проверьте, что при POST запросе `/api/consoles/` с пустыми данными '
+            'возвращаетe 400'
+        )
