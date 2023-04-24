53e42c1f8f16fa024bc2c45e54956d3e2dd56736
Evgeniy V <volochek93@yandex.ru>
2023-04-24 15:14:05 +0400

    update pep8

--- backend/games/admin.py
+++ backend/games/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Comment, FavoriteGame, Game, Genre, Review, ShoppingList, Tag
+from .models import (Comment, FavoriteGame, Game, 
+                     Genre, Review, ShoppingList, Tag)
 
 
 @admin.register(Tag)
