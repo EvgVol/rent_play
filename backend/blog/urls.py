0a31148d7b64d1d9bae6174ed59a4b998b4931df
Evgeniy V <volochek93@yandex.ru>
2023-05-02 16:41:21 +0400

    rewrite func view to class view

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 
 urlpatterns = [
-    path('/', views.blog, name='blog'),
-    path('/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('/', include([
+        path('', views.BlogListView.as_view(), name='blog-list'),
+        path('/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail')
+    ])),
+
+    # path('/', views.blog, name='blog'),
+    # path('/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,30 +1,28 @@
+from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from django.conf import settings
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 from games.models import Tag, Genre
 
 
-def blog(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 8)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    tags = Tag.objects.all()
-    genres = Genre.objects.all()
-    context = {'page_obj': page_obj,
-               'tags': tags,
-               'genres': genres}
-    template = 'blog/blog.html'
-    return render(request, template, context)
+class BlogListView(ListView):
+    """Отображает страницу блога с 8 постами."""
+
+    model = Post
+    template_name = "blog/blog.html"
+    paginate_by = 8
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        context['genres'] = Genre.objects.all()
+        return context
 
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    template = 'blog/single-post.html'
-    context = {
-        'author': post.author,
-        'post': post,
-    }
-    return render(request, template, context)
+class BlogDetailView(DetailView):
+    """Отображает страницу блога с данным постом"""
+
+    model = Post
+    template_name = "blog/single-post.html"
