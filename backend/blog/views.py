1ec56734a146788884544a3b0914163391931039
Evgeniy V <volochek93@yandex.ru>
2023-05-02 16:52:19 +0400

    update page blog-detail

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,3 @@
-from typing import Any, Dict
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
 from django.views.generic import ListView, DetailView
 
 from blog.models import Post
